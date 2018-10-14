f73e683305eb2f6cc084968969661bdafc85f263
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2018-10-14 12:43:42 +0100

    create a header component

---
+++ src/Base/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Header extends React.Component{
+  constructor (props) {
+    super()
+  }
+  render () {
+    return (
+      <div className="header">
+        <nav className="container">
+        
+        </nav>
+        <div className="hero">
+          <div className="container">
+            <h1>
+              Hey there * <br/>
+              My name is Adekoyejo. <br/>
+              I am a <span className="lighter">fullstack</span> backend developer.
+            </h1>
+            <p className="description">
+            I am currently building applications as a fullstack developer at Foodie Nigeria. I throughly enjoy working with nodejs and other backend technologies although I sometimes build frontend applications using HTML, CSS, Javascript.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
