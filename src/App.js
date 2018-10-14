97b751e26bb928938fd02d2c0a586e87139c9b5c
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2018-10-14 20:03:16 +0100

    add footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Header} from './Base'
+import { Footer } from './Footer'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
+        <Footer/>
       </div>
     );
   }
