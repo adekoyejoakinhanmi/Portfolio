88635efb837b6a5d93c30670896daf22c2208dac
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2018-10-14 21:55:23 +0100

    add seo options

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import {Header} from './Base'
 import { Footer } from './Footer'
+import ReactGA from 'react-ga'
 import './App.css';
+
+ReactGA.initialize('UA-127451777-1')
+ReactGA.pageview('/')
 
 class App extends Component {
   render() {
