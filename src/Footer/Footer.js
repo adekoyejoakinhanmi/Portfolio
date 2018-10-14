ba32e9d98899298bdf662d348a82c3421c7c7552
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2018-10-14 21:11:16 +0100

    add and style social media icon links

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithubSquare, FaTwitterSquare, FaMedium, FaLinkedin } from "react-icons/fa";
 
 export default class Footer extends React.Component{
   constructor (props) {
@@ -14,6 +15,12 @@ export default class Footer extends React.Component{
             <div className="contact-box">
               <p>Email Address</p>
               <a href="mailto:koyesavedbygrace@yahoo.com">koyesavedbygrace@yahoo.com</a>
+              <p className="social-icons">
+                <a href="https://www.linkedin.com/in/aakinhanmid/" target="blank"><FaLinkedin/></a>
+                <a href="https://www.github.com/adekoyejoakinhanmi" target="blank"><FaGithubSquare/></a>
+                <a href="https://www.twitter.com/senorNigeria" target="blank"><FaTwitterSquare/></a>
+                <a href="https://medium.com/@senorNigeria" target="blank"><FaMedium/></a>
+              </p>
             </div>
           </div>
           <p className="text-center">Built with &hearts; and React </p>
